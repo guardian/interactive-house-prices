83a4b5bbd6fec867ee242bea607949250246a178
Will Franklin <will.franklin@guardian.co.uk>
2015-06-08 16:05:12 +0100

    Add 2014 data for better testing

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -67,6 +67,7 @@ function init(el) {
     el.innerHTML = mainHTML;
 
     var map = L.map('map').setView([51.505, -0.09], 7);
+
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         minZoom: 7,
