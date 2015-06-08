62ad6d79d4b3ecc6ddca81abe0d1cc4cb67b77b7
Will Franklin <will.franklin@guardian.co.uk>
2015-06-08 12:46:25 +0100

    Added postcode areas topo

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,11 +1,13 @@
 import L from 'leaflet'
+import topojson from 'mbostock/topojson'
 
 import mainHTML from './text/main.html!text'
-import prices from './data/prices.json!json'
-import wages from './data/wages.json!json'
+import areasTopo from './data/areas-topo.json!json'
+//import prices from './data/prices.json!json'
+//import wages from './data/wages.json!json'
 
-const years = Object.keys(prices.mins).sort();
-const constituencies = Object.keys(prices.constituencies).filter(c => c[0] === 'E');
+//const years = Object.keys(prices.mins).sort();
+//const constituencies = Object.keys(prices.constituencies).filter(c => c[0] === 'E');
 
 var selectedConstituency;
 
@@ -36,8 +38,6 @@ function initTime(el, map) {
 
         yearEl.textContent = year;
         monthEl.textContent = month < 10 ? '0' + month : month;
-
-        return;
 
         constituencyPrices.forEach(constituency => {
             map.toggleConstituency(constituency.id, constituency.value < 6);
@@ -85,7 +85,7 @@ function initConstituencies(el, map) {
 
     constituencyEl.addEventListener('change', evt => {
         if (evt.target.value) {
-            //map.focusConstituency(evt.target.value);
+            map.focusConstituency(evt.target.value);
         }
         selectedConstituency = evt.target.value;
     });
@@ -104,8 +104,11 @@ function init(el) {
         accessToken: 'pk.eyJ1Ijoid3BmNTAwIiwiYSI6ImUwY2JiYmEzYzFhMjM1NDNmN2U3NDgxOTA0NDZkY2MwIn0.AheQI8clLR9rK_Auf7r8Sw'
     }).addTo(map);
 
-    initTime(el, map);
-    initConstituencies(el, map);
+    var feature = topojson.feature(areasTopo, areasTopo.objects.Areas);
+    L.geoJson(feature).addTo(map);
+
+    //initTime(el, map);
+    //initConstituencies(el, map);
 }
 
 (window.define || System.amdDefine)(function() { return {init: init}; });
