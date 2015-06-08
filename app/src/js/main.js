cc402405fede130ce967b2acf844c4b57ff6d51b
Will Franklin <will.franklin@guardian.co.uk>
2015-06-08 17:44:19 +0100

    Add place labels as another layer

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -7,6 +7,8 @@ import mainHTML from './text/main.html!text'
 import prices from './data/prices.json!json'
 import areasTopo from './data/areas-topo.json!json'
 
+const mbToken = 'pk.eyJ1IjoiZ3VhcmRpYW4iLCJhIjoiNHk1bnF4OCJ9.25tK75EuDdgq5GxQKyD6Fg';
+
 const postcodeAreas = Object.keys(prices);
 const areasFeature = topojson.feature(areasTopo, areasTopo.objects.Areas);
 areasFeature.features = areasFeature.features.filter(f => postcodeAreas.indexOf(f.id) !== -1);
@@ -20,12 +22,21 @@ function init(el) {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         minZoom: 7,
         maxZoom: 17,
-        id: 'wpf500.9a8b026d',
-        accessToken: 'pk.eyJ1Ijoid3BmNTAwIiwiYSI6ImUwY2JiYmEzYzFhMjM1NDNmN2U3NDgxOTA0NDZkY2MwIn0.AheQI8clLR9rK_Auf7r8Sw'
+        id: 'guardian.cd3f3254',
+        accessToken: mbToken
     }).addTo(map);
 
     var areasLayer = L.geoJson(undefined, {'className': 'map-area'}).addTo(map);
     areasLayer.addData(areasFeature);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        pane: 'overlayPane',
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        minZoom: 7,
+        maxZoom: 17,
+        id: 'guardian.8c876c82',
+        accessToken: mbToken
+    }).addTo(map);
 
     var changeTime = new ChangeTime(el, map, areasLayer);
 }
