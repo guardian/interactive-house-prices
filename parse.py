92e22501b95b2aa23ffa41ea51562dc5e2659116
Will Franklin <will.franklin@guardian.co.uk>
2015-06-09 10:52:47 +0100

    Reduce prices.json size

--- parse.py
+++ parse.py
@@ -6,6 +6,6 @@ postcode_areas = defaultdict(lambda: defaultdict(lambda: {}))
 
 for row in csv.DictReader(sys.stdin):
     avg = float(row['avg'])
-    postcode_areas[row['postcode'].strip()][row['year']][row['month']] = float(row['avg'])
+    postcode_areas[row['postcode'].strip()][row['year']][row['month']] = round(float(row['avg']), 2)
 
 print json.dumps(postcode_areas)
