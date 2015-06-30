df875f0df11b4251674dd5d60b1be568e2a68ea3
PommE15 <apple.chan-fardel@guardian.co.uk>
2015-06-30 10:31:48 +0100

    Add colours to tooltip

--- app/src/js/components/tooltip.js
+++ app/src/js/components/tooltip.js
@@ -1,8 +1,8 @@
-import { getRegionPrices } from '../lib/region'
-import debounce from '../lib/debounce'
+import { getRegionPrices } from '../lib/region';
+import debounce from '../lib/debounce';
 
-import template from './templates/tooltip.html!text'
-import areaName from '../data/areas-name.json!json'
+import template from './templates/tooltip.html!text';
+import areaName from '../data/areas-name.json!json';
 
 const min = 7000;
 const max = 50000000;
@@ -15,15 +15,25 @@ export default class Tooltip {
         this.el = root.querySelector('.js-tooltip');
         this.el.innerHTML = template;
 
-        this.regionEl = this.el.querySelector('.js-region');
-        this.resultEl = this.el.querySelector('.js-result');
-        this.range1El = this.el.querySelector('.js-range1');
-        this.range2El = this.el.querySelector('.js-range2');
+        this.areaEl = this.el.querySelector('.js-area');
+        this.districtEl = this.el.querySelector('.js-district');
+        
         this.factorEl = this.el.querySelector('.js-factor');
-        this.minEl = this.el.querySelector('.js-min');
-        this.avgEl = this.el.querySelector('.js-avg');
-        this.maxEl = this.el.querySelector('.js-max');
         this.salaryEls = Array.from(this.el.querySelectorAll('.js-salary'));
+        this.medEls = Array.from(this.el.querySelectorAll('.js-med'));
+        
+        this.minEl = this.el.querySelector('.js-min');
+        this.maxEl = this.el.querySelector('.js-max');
+        this.avgEl = this.el.querySelector('.js-avg');
+        
+        this.range1El = this.el.querySelector('.range-pipe-l');
+        this.range2El = this.el.querySelector('.range-pipe-s');
+        this.range3El = this.el.querySelector('.range-pipe-r');
+        
+        this.marker1El = this.el.querySelector('.marker-salary');
+        this.marker2El = this.el.querySelector('.marker-med');
+        this.marker3El = this.el.querySelector('.marker-min');
+        //this.markerEls = Array.from(this.el.querySelectorAll('.marker'));
 
         var resize = debounce(function () {
             window.requestAnimationFrame(() => {
@@ -42,26 +52,44 @@ export default class Tooltip {
     }
 
     show(evt, data) {
-        var region = evt.target.feature;
-        var prices = getRegionPrices(region, data.year, data.month);
 
-        var salary = data.threshold;
-        var fac = Math.round(prices.med / salary);
+        var districtObj = evt.target.feature,
+            district = districtObj.id,
+            area = areaName[district.replace(/[0-9].*/,'')] + " area";
+        
+        var prices = getRegionPrices(districtObj, data.year, data.month),
+            salary = data.threshold,
+            factor = prices.med / salary;
 
-        var result = Math.round(salary*fac).toLocaleString(),
-            range1 = Math.round(salary*6).toLocaleString(),
-            range2 = Math.round(salary*12).toLocaleString();
+        var ratio = 100 / prices.max,
+            range1 = ratio * salary * 4,
+            range2 = ratio * salary * 14,
+            range3 = 100 - range1 - range2;
+        
+        //console.log(district);
+        //console.log(prices);
+        
+        // header
+        this.areaEl.textContent = area; 
+        this.districtEl.textContent = district; 
+        
+        // summary
+        this.marker1El.style.left = ratio*salary + "%";        
+        this.marker2El.style.left = ratio*prices.med + "%";        
+        this.marker3El.style.left = ratio*prices.min + "%";        
 
-        this.regionEl.textContent = `${areaName[region.id.replace(/[0-9].*/, '')]} area [${region.id}]`;
-        this.resultEl.textContent = result;
-        this.range1El.textContent = range1;
-        this.range2El.textContent = range2;
-        this.factorEl.textContent = fac.toLocaleString();
         this.minEl.textContent = prices.min.toLocaleString();
-        this.avgEl.textContent = prices.avg.toLocaleString();
         this.maxEl.textContent = prices.max.toLocaleString();
+      //this.avgEl.textContent = prices.avg.toLocaleString();
+        this.factorEl.textContent = factor.toLocaleString();
+        
+        this.medEls.forEach(el => el.textContent = prices.med.toLocaleString());
         this.salaryEls.forEach(el => el.textContent = salary.toLocaleString());
-
+        
+        this.range1El.style.width = range1 + "%";        
+        this.range2El.style.width = range2 + "%";        
+        //this.range3El.style.width = range3 + "%";        
+        
         var x = evt.containerPoint.x;
         var y = evt.containerPoint.y;// - tooltipHeight;
         if (x + tooltipWidth > this.viewWidth) {
