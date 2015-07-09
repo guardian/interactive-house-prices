6fa6d580142fd12d323d745a3251921a332240d3
Will Franklin <will.franklin@guardian.co.uk>
2015-07-09 12:12:37 +0100

    Work on intro screen

--- app/src/js/components/intro.js
+++ app/src/js/components/intro.js
@@ -3,6 +3,20 @@ import { config } from '../lib/cfg'
 
 export default class Intro {
     constructor(el) {
-        el.innerHTML = template.replace(/%assetPath%/g, config.assetPath);
+        el.innerHTML = template;
+        this.steps = [1, 2, 3, 4].map(n => {
+            return {
+                'img': el.querySelector('.js-img-' + n),
+                'year': el.querySelector('.js-year-' + n)
+            };
+        });
+        var img = new Image();
+        img.onload = this.start.bind(this);
+        img.src = config.assetPath + '/assets/intro.png';
+        console.log(this.steps);
+    }
+
+    start() {
+        
     }
 }
