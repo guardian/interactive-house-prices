87d0328a5d8fe44fba95432b5d233c8530ad959c
Will Franklin <will.franklin@guardian.co.uk>
2015-07-10 14:48:05 +0100

    Some intro stuff

--- app/src/js/components/intro.js
+++ app/src/js/components/intro.js
@@ -2,16 +2,19 @@ import template from './templates/intro.html!text'
 import { config } from '../lib/cfg'
 import { startYear } from '../lib/region'
 
-const delay = 400;
-const groups = [3, 5, 5, 7];
+const textDelay = 50;
+const text = Array.from('If you earned the national average salary, this is where you could live.');
+const textCount = text.length;
 
-const stepNos = Array.prototype.concat.apply([], groups.map((n, i) => new Array(n).fill(i)));
-const stepCount = stepNos.length;
+const imgDelay = 400;
+const imgGroups = [3, 5, 5, 7];
+const imgNos = Array.prototype.concat.apply([], imgGroups.map((n, i) => new Array(n).fill(i)));
+const imgCount = imgNos.length;
 
 export default class Intro {
     constructor(el) {
         el.innerHTML = template;
-        this.steps = groups.map((n, i) => {
+        this.imgs = imgGroups.map((n, i) => {
             var img = el.querySelector('.js-img-' + i);
             return {
                 'img': img,
@@ -20,24 +23,46 @@ export default class Intro {
         });
 
         var img = new Image();
-        img.onload = this.step.bind(this, 0);
+        img.onload = this.imgTick.bind(this, 0);
         img.src = config.assetPath + '/assets/intro.png';
+
+        var step0 = el.querySelector('.js-step-0');
+        this.textEls = text.map(t => {
+            var el = document.createElement('span');
+            el.textContent = t;
+            el.className = 'intro__text';
+            step0.appendChild(el);
+            return el;
+        });
+
+        console.log(this.textEls);
+
+        this.textTick(0);
     }
 
-    step(n) {
+    textTick(n) {
         window.requestAnimationFrame(() => {
-            var stepNo = stepNos[n];
-            var group = this.steps[stepNo];
+            this.textEls[n].className += ' is-visible';
+            if (n + 1 < textCount) {
+                setTimeout(this.textTick.bind(this, n + 1), textDelay);
+            }
+        });
+    }
 
-            group.img.style.backgroundPositionX = (n / (stepCount - 1) * 100) + '%';
-            if (stepNo != this.lastStepNo) {
+    imgTick(n) {
+        window.requestAnimationFrame(() => {
+            var imgNo = imgNos[n];
+            var group = this.imgs[imgNo];
+
+            group.img.style.backgroundPositionX = (n / (imgCount - 1) * 100) + '%';
+            if (imgNo != this.lastStepNo) {
                 group.img.style.display = 'block';
-                this.lastStepNo = stepNo;
+                this.lastStepNo = imgNo;
             }
             group.year.textContent = startYear + n;
 
-            if (n + 1 < stepCount) {
-                setTimeout(this.step.bind(this, n + 1), delay);
+            if (n + 1 < imgCount) {
+                setTimeout(this.imgTick.bind(this, n + 1), imgDelay);
             }
         });
     }
