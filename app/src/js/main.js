169d0944827a10292d37de544b7d08fde8fe529e
Will Franklin <will.franklin@guardian.co.uk>
2015-07-28 11:27:24 +0100

    Remove intro

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -4,7 +4,6 @@ import './lib/raf'
 
 import { set as setConfig } from './lib/cfg'
 
-import Intro from './components/intro'
 import Map from './components/map'
 import User from './components/user'
 
@@ -14,13 +13,6 @@ export function init(el, config) {
     setConfig(config);
 
     el.innerHTML = mainHTML;
-
-    // While testing
-    if (window.location.hash === '#intro') {
-        var intro = new Intro(el.querySelector('.js-intro'));
-    } else {
-        el.querySelector('.js-intro').style.display = 'none';
-    }
 
     var map = new Map(el.querySelector('.js-map'));
     var user = new User(el.querySelector('.js-user'), map.update);
