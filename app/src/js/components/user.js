12525918085a9ab9632e70f6f241c0021a56e844
Will Franklin <will.franklin@guardian.co.uk>
2015-07-28 15:31:22 +0100

    Make share buttons work

--- app/src/js/components/user.js
+++ app/src/js/components/user.js
@@ -1,10 +1,11 @@
 import template from './templates/user.html!text'
+
 import throttle from '../lib/throttle'
 import { startYear, endYear, getPeriodSplits } from '../lib/region'
 import { config } from '../lib/cfg'
-
 import madlib from '../lib/madlib'
 import range from '../lib/range'
+import share from '../lib/share'
 
 import Linechart from './linechart'
 
@@ -24,8 +25,10 @@ export default function User(el, onUpdate) {
         ratioEl = el.querySelector('.js-user-ratio');
         thumblineEl = document.querySelector('.hp-range-slider__thumbline');
 
-        range(el.querySelector('.js-date'), startYear, endYear, changeYear, 5);
         madlib(el.querySelector('.js-wage'), changeThreshold);
+
+        linechart = new Linechart('js-line', 'line', 266, 55, 5, 0);
+        range(el.querySelector('.js-date'), startYear, endYear, changeYear, 5);
 
         minimap = el.querySelector('.js-minimap');
         for (year = startYear; year <= endYear; year++) {
@@ -35,14 +38,18 @@ export default function User(el, onUpdate) {
         }
         minimapImgs[endYear].style.display = 'block';
 
-        linechart = new Linechart('js-line', 'line', 266, 55, 5, 0);
+        [].slice.call(el.querySelectorAll('.js-share')).forEach(shareEl => {
+            var network = shareEl.getAttribute('data-network');
+            shareEl.addEventListener('click', () => {
+                var msg = ratioEl.textContent + ' of the country is beyond my means in ' + currentValue.year + '. ';
+                share(network, msg);
+            });
+        });
+
+        window.addEventListener('resize', () => { isMobile = window.innerWidth < 740; });
+        isMobile = window.innerWidth < 740;
 
         changeThreshold(25000); // ugly way to initialise line chart
-
-        window.addEventListener('resize', () => {
-            isMobile = window.innerWidth < 740;
-        });
-        isMobile = window.innerWidth < 740;
     }
 
     function change(type) {
